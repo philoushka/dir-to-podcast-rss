e7e4303b32b9fe7c58df99aa85e16800193a8d2c
Phil Campbell <philoushka@gmail.com>
2014-07-23 22:37:13 -0700

    initial checkin with absolute 0 functionality

---
+++ DIY-PodcastRss/Controllers/RssController.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+namespace DIY_PodcastRss.Controllers {
+    public class RssController : Controller {
+
+        public SyndicationFeedResult Feed() {
+            var feed = new SyndicationFeed();
+
+            return new SyndicationFeedResult(feed);
+        }
+
+    }
+
+    public class SyndicationFeedResult : ContentResult {
+        public SyndicationFeedResult(SyndicationFeed feed)
+            : base() {
+            string rssXml = new DIYPodcastRss.Core.SyndicationFeedResult().GenerateRssXml(feed);
+            Content = rssXml;
+            ContentType = "application/rss+xml";
+        }
+    }
+}
